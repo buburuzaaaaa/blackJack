ba531d71d4b35be98e1525f565383935973e0b2f
buburuzaaaaa <andrew4joy2015@gmail.com>
2023-10-10 15:16:38 -0700

    frontend update

---
+++ public/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+      $("h1").hide().delay(500).fadeIn(2000).show(2000).end();
+})  
+$(document).ready(function(){
+    $(".ply").hide().delay(900).fadeIn(2000).show(2000).end()
+});
