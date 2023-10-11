2bae01a170f283765daf9e7f41490c57f8d8ec63
buburuzaaaaa <andrew4joy2015@gmail.com>
2023-10-11 15:15:44 -0700

    frontend changes

--- public/script.js
+++ public/script.js
@@ -1,6 +1,27 @@
+const login = document.getElementById('bt');
+// jquery for frontend
 $(document).ready(function(){
       $("h1").hide().delay(500).fadeIn(2000).show(2000).end();
 })  
 $(document).ready(function(){
-    $(".ply").hide().delay(900).fadeIn(2000).show(2000).end()
+    $(".btn").hide().delay(900).fadeIn(2000).show(2000).end()
 });
+$(document).ready(function(){
+    $(".txt-bx").hide().delay(900).fadeIn(2000).show(2000).end()
+});
+$(document).ready(function(){
+    $(".btn1").hide().delay(900).fadeIn(2000).show(2000).end()
+});
+
+// for login button if clicked
+// button.addEventListener('click', function(){
+//     if(login == true){
+//         $(document).ready(function(){
+//             $(".txt-bx").fadeIn(2000).end()
+//         });
+//     }
+// });
+
+
+
+
