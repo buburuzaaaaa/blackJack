e5349986b816322a83203ce6e2682f494503477c
Dearborn42 <amurf26@outlook.com>
2023-10-17 15:17:59 -0700

    Updated game functions for more universal use

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -11,6 +11,39 @@ const getOption = {
 }
 
 
+// function to draw specific amount of cards
+async function drawCards(count, req){
+    const {deck_id} = req.session;
+    const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${count}`
+    const response = await fetch(url, getOption);
+    const card = await response.json();
+    return card
+}
+
+// function to display and update interface for players
+export function display(req, res){
+    const {player, dealer} = req.session;
+    res.render(path.join(__dirname, '../public/game.ejs'), {player:player, dealer:dealer});
+}
+
+// function to give cards to specific players or dealer
+async function assignCard(req, check){
+    const {player, dealer} = req.session;
+    var card = await drawCards(1, req);
+    if(check === true) 
+        player.cards.push(card.cards[0])
+    else{
+        card.cards[0].side = "https://www.deckofcardsapi.com/static/img/back.png"
+        dealer.cards.push(card.cards[0]);
+        for(let i=0; i<dealer.cards.length; i++){
+            if(i != dealer.cards.length-1)
+                dealer.cards[i].side = "";
+        }
+    }
+}
+
+
+// start function, creates deck from api and sets session data
 export async function deck(req, res, next){
     const url = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
     const response = await fetch(url, getOption)
@@ -20,28 +53,22 @@ export async function deck(req, res, next){
     req.session.deck_id = deck.deck_id;
     req.session.player = {cards: [], value: 0}
     req.session.dealer = {cards: [], value: 0}
-    await assingCards(4, req);
+    await startingHand(req, res);
     res.render(path.join(__dirname, "../public/game-start.ejs"), {});
 }
-
-export async function assingCards(count, req){
-    const {deck_id, player, dealer} = req.session;
-    const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${count}`
-    const response = await fetch(url, getOption);
-    const cards = await response.json();
-    dealer.cards.forEach((x)=>{x.side=""})
-    for(let i=0; i<cards.cards.length; i++) {
-        cards.cards[i].side = "";
-        if(i%2 == 0){
-            player.cards.push(cards.cards[i]);
-        }else{
-            if(i == cards.cards.length-1)
-                cards.cards[i].side = "https://www.deckofcardsapi.com/static/img/back.png"
-            dealer.cards.push(cards.cards[i]);
-        }
+// part of deck function, deals out initial cards
+export async function startingHand(req, res){
+    for(let i=0; i<4; i++) {
+        if(i%2==0)
+            await assignCard(req, true)
+        else
+            await assignCard(req, false)
     }
 }
 
+
+// counts value of cards and sets session data accordingly
+// also checks certain win conditions
 export async function getValue(req, res, next){
     const {player, dealer} = req.session;
     player.value = 0;
@@ -57,44 +84,56 @@ export async function getValue(req, res, next){
                 person.value += Number(card.value);
         })
     })
-    if(player.value === 21){
+    console.log(player.value, dealer.value)
+    if(player.value === 21 || dealer.value > 21){
         await user.findOneAndUpdate({ password: req.user.password }, { $inc: { win: 1 } }, { new: true });
         return res.render(path.join(__dirname, "../public/result.ejs"), {result: "won"});
-    }
-    if(player.value > 21){
+    }else if(player.value > 21 || dealer.value === 21){
         await user.findOneAndUpdate({ password: req.user.password }, { $inc: { loss: 1 } }, { new: true });
         return res.render(path.join(__dirname, "../public/result.ejs"), {result: "lost"});
     }
     next();
 }
 
-
+// Function to hit, will assign cards to players and/or dealer if dealer has less than 17 points
 export async function hit(req, res, next){
     const {dealer} = req.session;
     if(dealer.value >= 17)
-        await assingCards(1, req);
-    else
-        await assingCards(2, req);
+        await assignCard(req, true);
+    else{
+        await assignCard(req, true);
+        await assignCard(req, false);
+    }
     next();
 }
 
+// Function to stand, will check if dealer wins or give dealer a card 
 export async function stand(req, res, next){
     const {deck_id, dealer} = req.session;
     if(dealer.value >= 17)
-        next();
+        await win(req, res)
     else{
-        const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
-        const response = await fetch(url, getOption);
-        const cards = await response.json();
-        dealer.cards.forEach((x)=>{x.side = ""})
-        cards.cards.side = "https://www.deckofcardsapi.com/static/img/back.png";
-        dealer.cards.push(cards.cards);
+        await assignCard(req, res, false)
         next();
-    }
+    }    
 }
 
-
-export function display(req, res){
+// checks if anyone wins after a stand
+async function win(req, res){
     const {player, dealer} = req.session;
-    res.render(path.join(__dirname, '../public/game.ejs'), {player:player, dealer:dealer});
+    var result;
+    if(player.value>dealer.value){
+        await user.findOneAndUpdate({ password: req.user.password }, { $inc: { win: 1 } }, { new: true });
+        result = "won";
+    }else if(player.value===dealer.value){
+        result = "tie";
+    }else if(player.value<dealer.value){
+        await user.findOneAndUpdate({ password: req.user.password }, { $inc: { loss: 1 } }, { new: true });
+        result = "lost";
+    }
+    return res.render(path.join(__dirname, "../public/result.ejs"), {result: result});
 }
+
+
+
+
